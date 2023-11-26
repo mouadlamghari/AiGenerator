2cfd7c0175ea5c78bc0d53c47ef5a4e24a11c74d
mouadLLL <mouaddev0002@gmail.com>
2023-11-26 11:20:28 +0100

    b

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "AiGenerator/client/"
-
+  base: '/AiGenerator/client/',
+  build: {
+    outDir: '../docs', // This is important for GitHub Pages
+  },
 })
+
